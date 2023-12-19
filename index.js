ccc40dce42a5885958a8e8a0a5b92a4fa28d3ae0
Taha Arbas <129167579+TahaArbass@users.noreply.github.com>
2023-12-19 13:39:07 +0200

    sign up patient done

--- index.js
+++ index.js
@@ -13,7 +13,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 // app.use(bodyParser.json());
-
 app.set('view engine', 'ejs');
 
 // getting routes
